bb5e339358466014c2f0175ed14a2587ab4a15a6
tiffanychu90 <tiffany.chu@lacity.org>
2019-11-19 23:03:44 +0000

    Change CARE data from S3 to database

--- src/clean-care-311.py
+++ src/clean-care-311.py
@@ -12,23 +12,13 @@ bucket_name = 's3://city-of-los-angeles-data-lake/public-health-dashboard/'
 
 s3 = boto3.client('s3')
 
-df = pd.read_csv(f'{bucket_name}data/raw/MyLA311_Service_Requests.csv')
+df = catalog.care311.read()
 
-for col in ['CreatedDate', 'UpdatedDate', 'ClosedDate', 'ServiceDate']:
+for col in ['createddate', 'updateddate', 'closeddate', 'servicedate']:
     df[col] = pd.to_datetime(df[col])
-    
 
-# Create geometry column from lat/long
-df['geometry'] = df.dropna(subset=['Latitude', 'Longitude']).apply(
-    lambda x: Point(x.Longitude, x.Latitude), axis = 1)
-
-
-# Convert to GeoDataFrame
-df = gpd.GeoDataFrame(df)
-df.crs = {'init':'epsg:4326'}
-
-df = df[df.geometry.notna()]
-df = df.to_crs({'init':'epsg:2229'})
+# Extract year
+df['year'] = df['createddate'].dt.year
 
 
 #-----------------------------------------------------------#
@@ -42,16 +32,16 @@ def request_type(row):
     illegal = 0
     other = 0
     
-    if row.RequestType == 'Homeless Encampment':
+    if row.requesttype == 'Homeless Encampment':
         homeless = 1
-    elif (row.RequestType == 'Bulky Items'):
+    elif (row.requesttype == 'Bulky Items'):
         bulky = 1
-    elif 'Illegal Dumping' in row.RequestType:
+    elif 'Illegal Dumping' in row.requesttype:
         illegal = 1
-    elif ((row.RequestType == 'Service Not Complete') or 
-          (row.RequestType == 'Metal/Household Appliances') or 
-          (row.RequestType == 'Electronic Waste') or 
-          (row.RequestType == 'Dead Animal Removal')):
+    elif ((row.requesttype == 'Service Not Complete') or 
+          (row.requesttype == 'Metal/Household Appliances') or 
+          (row.requesttype == 'Electronic Waste') or 
+          (row.requesttype == 'Dead Animal Removal')):
         other = 1
     
     return pd.Series([homeless, bulky, illegal, other], index=['homeless', 'bulky', 'illegal', 'other'])
@@ -59,17 +49,15 @@ def request_type(row):
 requests = df.apply(request_type, axis = 1)
 df = pd.concat([df, requests], axis = 1)
 
-
-# Extract year 
-for col in ['CreatedDate', 'UpdatedDate']:
-    df[col] = pd.to_datetime(df[col])
-
+# Drop if geom is null
+df = df[df.geom.notna()]
+    
     
 #-----------------------------------------------------------#
 # Spatial join with census tracts
 #-----------------------------------------------------------#
-tracts = gpd.read_file(f'{bucket_name}gis/raw/census_tracts.geojson')
-df['year'] = df.CreatedDate.dt.year
+tracts = gpd.read_file(f'{bucket_name}gis/raw/census_tracts.geojson').to_crs({'init':'epsg:4326'})
+
 
 m1 = gpd.sjoin(df, tracts, how = 'inner', op = 'intersects')
 
@@ -81,11 +69,11 @@ pivot1 = m1.pivot_table(index = ['GEOID', 'year'],
 pivot1 = pd.merge(pivot1, tracts, on = 'GEOID', validate = 'm:1')
 
 pivot1 = gpd.GeoDataFrame(pivot1)
-pivot1.crs = {'init':'epsg:2229'}
+pivot1.crs = {'init':'epsg:4326'}
 
-
+            
 # Export to S3
-pivot1.to_file(driver = 'GeoJSON', filename = './gis/care311_tracts.geojson')
+pivot1.to_crs({'init':'epsg:2229'}).to_file(driver = 'GeoJSON', filename = './gis/care311_tracts.geojson')
 
 s3.upload_file('./gis/care311_tracts.geojson', 'city-of-los-angeles-data-lake', 
                'public-health-dashboard/gis/intermediate/care311_tracts.geojson')
