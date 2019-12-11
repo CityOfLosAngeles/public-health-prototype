6ff513be4f9ca44fb1dd4d24f5ad0fde3c52264b
hunterowens <owens.hunter@gmail.com>
2019-12-11 22:06:56 +0000

    load data

--- dashboard/load_data.R
+++ dashboard/load_data.R
@@ -24,9 +24,57 @@ con <- dbConnect(RPostgres::Postgres(),
                  user = username,
                  password = password)
 
-
-cases <- tbl(con, dbplyr::in_schema('"public-health"','"311-cases-homelessness"')) %>% collect()
-
-cases$closeddate <- as_date(cases$closeddate) 
-
-cases <- cases %>% drop_na('closeddate')
+load_data <- function() {
+  data <- tbl(con, dbplyr::in_schema('"public-health"','"311-cases-homelessness"')) %>% collect()
+  
+  data$closeddate <- as_date(data$closeddate) 
+  
+  data <- data %>% drop_na('closeddate')
+  
+  #' This script loads the data files and ensures the correct data types are used
+  
+  # column names with proper spacing / underscores 
+  col_names_311 <- c(
+    "srn_number", "created_date", "updated_date", "action_taken",
+    "owner", "request_type", "status", "request_source",
+    "mobile_os", "anonymous", "assign_to", "service_date",
+    "closed_date", "address_verified", "approximate_address",
+    "address", "house_number", "direction", "street_name",
+    "suffix", "zipcode", "latitude", "longitude", "location",
+    "thompson_brothers_map_page", "thompson_brothers_map_column",
+    "thompson_brothers_map_row", "area_planning_commissions",
+    "council_district", "city_council_member",
+    "neighborhood_council_code", "neighborhood_council_name",
+    "police_precinct"
+  )
+  
+  data <- data %>% 
+           select(-c('index')) %>%
+           rename(
+                  'action_taken' = 'actiontaken',
+                  'address_verified' = 'addressverified',
+                  'approximate_address' = 'approximateaddress',
+                  'assign_to' = 'assignto',
+                  'council_district' = 'cd',
+                  'cd_member' = 'cdmember',
+                  'closed_date' = 'closeddate',
+                  'created_by_user_organization' = 'createdbyuserorganization',
+                  'created_date' = 'createddate',
+                  'date_service_rendered' = 'dateservicerendered',
+                  'house_number' = 'housenumber',
+                  'mobile_os' = 'mobileos',
+                  'neighborhood_council_name' = 'ncname',
+                  'police_precinct' = 'policeprecinct',
+                  'reason_code' = 'reasoncode',
+                  'request_source' = 'requestsource',
+                  'request_type' = 'requesttype',
+                  'resolution_code' = 'resolutioncode', 
+                  'service_date' = 'servicedate',
+                  'service_request_number' = 'srnumber',
+                  'street_name' = 'streetname',
+                  'updated_date' = 'updateddate'
+                  )
+  data$closed_date <- data$closed_date %>% as_datetime()
+  data$created_date <- data$created_date %>% as_datetime()
+  return(data)
+}
