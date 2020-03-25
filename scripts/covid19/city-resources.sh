7efab9babc12f053f101e38dcf5e331b985defc6
ian-r-rose <ian.r.rose@gmail.com>
2020-03-25 14:44:58 -0700

    Improve output.

--- scripts/covid19/city-resources.sh
+++ scripts/covid19/city-resources.sh
@@ -12,20 +12,22 @@ TIER_2_SHELTERS_URL="https://services1.arcgis.com/X1hcdGx5Fxqn4d0j/ArcGIS/rest/s
 # are incorrectly interpreted as times.
 SENIOR_NUTRITION_URL="https://services5.arcgis.com/7nsPwEMP38bSkCjy/arcgis/rest/services/Senior_Nutrition_Dining_Sites_COVID19_20200325/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=json&token="
 
+echo "Downloading resources"
+
 rm -f grabngo.kmz
-curl -J -L $GRAB_AND_GO_URL -o grabngo.kmz
+curl -s -J -L $GRAB_AND_GO_URL -o grabngo.kmz
 
 rm -f handwashing.geojson
-curl -J -L $HANDWASHING_STATIONS_URL -o handwashing.geojson
+curl -s -J -L $HANDWASHING_STATIONS_URL -o handwashing.geojson
 
 rm -f tier1.geojson
-curl -J -L $TIER_1_SHELTERS_URL -o tier1.geojson
+curl -s -J -L $TIER_1_SHELTERS_URL -o tier1.geojson
 
 rm -f tier2.geojson
-curl -J -L $TIER_2_SHELTERS_URL -o tier2.geojson
+curl -s -J -L $TIER_2_SHELTERS_URL -o tier2.geojson
 
 rm -f seniors.json
-curl -J -L $SENIOR_NUTRITION_URL -o seniors.json
+curl -s -J -L $SENIOR_NUTRITION_URL -o seniors.json
 
 rm -f $OUTFILE
 
@@ -35,13 +37,15 @@ echo "Assembling layers into $OUTFILE"
 # LAUSD Grab and Go Food Centers #
 ##################################
 
+echo "Writing Grab and Go Layer"
+
 # Merge the sublayers into one
 rm -f grabngo-merged.kmz
 ogrmerge.py \
     -f "LIBKML" \
     -single \
     -nln "LAUSD Grab & Go Food Centers" \
-    -o grabngo-merged.kml \
+    -o grabngo-merged.kmz \
     grabngo.kmz
 
 # Convert to GeoJSON for the sole purpose of dropping
@@ -51,7 +55,6 @@ ogr2ogr \
     -f "GeoJSON" \
     grabngo.geojson \
     grabngo-merged.kml
-
 # Add to the output layer with our own styling.
 ogr2ogr \
     -f "LIBKML" \
@@ -62,6 +65,8 @@ ogr2ogr \
 #############################
 # Handwashing station layer #
 #############################
+
+echo "Writing Handwashing Layer"
 
 # Add to the output layer, including our own styling.
 LIBKML_NAME_FIELD="Descriptio" \
@@ -76,6 +81,8 @@ LIBKML_NAME_FIELD="Descriptio" \
 ############################
 # Emergency shelters layer #
 ############################
+
+echo "Writing Emergency Shelter Layer"
 
 # Merge the tier 1 and tier 2 shelters.
 rm -f shelters.geojson
@@ -100,6 +107,8 @@ LIBKML_NAME_FIELD="Location" \
 ##################################
 # Senior nutrition centers layer #
 ##################################
+
+echo "Writing Senior Nutrition Layer"
 
 # Add to out output layer, including styling.
 LIBKML_NAME_FIELD="NAME" \
